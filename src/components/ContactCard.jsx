5dcaa1be3be55f5d6228e44b123f31458912d793
Vinoth6194 <vinothhkumar.cse.s@gmail.com>
2021-05-03 19:33:40 +0530

    refractrored list render with contact card comp

--- src/components/ContactCard.jsx
+++ src/components/ContactCard.jsx
@@ -1,7 +1,17 @@
 import React from 'react';
 
-function ContactCard() {
-  return <div></div>;
+function ContactCard(props) {
+  return (
+    <div>
+      <div className="item">
+        <div className="content">
+          <div className="header">{props.contact.name}</div>
+          <div>{props.contact.email}</div>
+        </div>
+        <i className="trash alternate outline icon"></i>
+      </div>
+    </div>
+  );
 }
 
 export default ContactCard;
