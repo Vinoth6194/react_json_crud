d5f875db269393ba1b92247202e6513f17be2f51
Vinoth6194 <vinothhkumar.cse.s@gmail.com>
2021-05-03 19:49:22 +0530

    set the initial contact list

--- src/components/App.js
+++ src/components/App.js
@@ -1,21 +1,23 @@
+import { useState } from 'react';
 import AddContact from './AddContact';
 import './App.css';
 import ContactList from './ContactList';
 import Header from './Header';
 
 function App() {
-  const contacts = [
-    {
-      id: '1',
-      name: 'Vinothkumar',
-      email: 'vin6194@gmail.com',
-    },
-    {
-      id: '2',
-      name: 'Kiruthika',
-      email: 'kiru1849@gmail.com',
-    },
-  ];
+  const [contacts, setContacts] = useState([]);
+  // const contacts = [
+  //   {
+  //     id: '1',
+  //     name: 'Vinothkumar',
+  //     email: 'vin6194@gmail.com',
+  //   },
+  //   {
+  //     id: '2',
+  //     name: 'Kiruthika',
+  //     email: 'kiru1849@gmail.com',
+  //   },
+  // ];
   return (
     <div className="ui container">
       <Header />
