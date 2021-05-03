820398ab85e3f3eec038c78df50523f07c69b2c5
Vinoth6194 <vinothhkumar.cse.s@gmail.com>
2021-05-03 19:20:52 +0530

    passed dummy contacts as prop

--- src/components/App.js
+++ src/components/App.js
@@ -4,11 +4,23 @@ import ContactList from './ContactList';
 import Header from './Header';
 
 function App() {
+  const contacts = [
+    {
+      id: '1',
+      name: 'Vinothkumar',
+      email: 'vin6194@gmail.com',
+    },
+    {
+      id: '2',
+      name: 'Kiruthika',
+      email: 'kiru1849@gmail.com',
+    },
+  ];
   return (
     <div className="ui container">
       <Header />
       <AddContact />
-      <ContactList />
+      <ContactList contacts={contacts} />
     </div>
   );
 }
