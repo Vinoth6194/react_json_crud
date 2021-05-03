8ffd1d3dd893b593eef37143286461fadea90caf
Vinoth6194 <vinothhkumar.cse.s@gmail.com>
2021-05-03 21:02:09 +0530

    delete func implmented

--- src/components/ContactCard.jsx
+++ src/components/ContactCard.jsx
@@ -13,6 +13,7 @@ function ContactCard(props) {
       <i
         className="trash alternate outline icon"
         style={{ color: 'red', marginTop: '7px' }}
+        onClick={() => props.clickHandler(id)}
       ></i>
     </div>
   );
