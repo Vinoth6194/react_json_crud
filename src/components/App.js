4e4c1c1ccbd68f4d1c74ffe8a53e3905419b7169
Vinoth6194 <vinothhkumar.cse.s@gmail.com>
2021-05-03 20:49:52 +0530

    add uid to the contact item

--- src/components/App.js
+++ src/components/App.js
@@ -3,14 +3,14 @@ import AddContact from './AddContact';
 import './App.css';
 import ContactList from './ContactList';
 import Header from './Header';
-
+import { uuid } from 'uuidv4';
 function App() {
   const LOCAL_STORAGE_KEY = 'contacts';
   const [contacts, setContacts] = useState([]);
   const addContactHandler = contact => {
     console.log('From App.js');
     console.log(contact);
-    setContacts([...contacts, contact]);
+    setContacts([...contacts, { id: uuid(), ...contact }]);
   };
 
   useEffect(() => {
