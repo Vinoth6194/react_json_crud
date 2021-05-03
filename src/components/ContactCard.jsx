6d608eb16c02556761e5753f66a8b62880ced7e3
Vinoth6194 <vinothhkumar.cse.s@gmail.com>
2021-05-03 19:38:26 +0530

    styles for icon and items

--- src/components/ContactCard.jsx
+++ src/components/ContactCard.jsx
@@ -3,14 +3,15 @@ import React from 'react';
 function ContactCard(props) {
   const { id, name, email } = props.contact;
   return (
-    <div>
-      <div className="item">
-        <div className="content">
-          <div className="header">{name}</div>
-          <div>{email}</div>
-        </div>
-        <i className="trash alternate outline icon"></i>
+    <div className="item">
+      <div className="content">
+        <div className="header">{name}</div>
+        <div>{email}</div>
       </div>
+      <i
+        className="trash alternate outline icon"
+        style={{ color: 'red', marginTop: '7px' }}
+      ></i>
     </div>
   );
 }
