0e1f8607921a62b48aa090ba1cd0f2831afc24e8
Vinoth6194 <vinothhkumar.cse.s@gmail.com>
2021-05-03 19:34:36 +0530

    destructuring the contact obj

--- src/components/ContactCard.jsx
+++ src/components/ContactCard.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
 function ContactCard(props) {
+  const { id, name, email } = props.contact;
   return (
     <div>
       <div className="item">
         <div className="content">
-          <div className="header">{props.contact.name}</div>
-          <div>{props.contact.email}</div>
+          <div className="header">{name}</div>
+          <div>{email}</div>
         </div>
         <i className="trash alternate outline icon"></i>
       </div>
