b622b8d6c84c14d7e5fc7d8547a62b390b3261ad
Vinoth6194 <vinothhkumar.cse.s@gmail.com>
2021-05-04 17:48:33 +0530

    set a link to delete

--- src/components/ContactCard.jsx
+++ src/components/ContactCard.jsx
@@ -14,11 +14,12 @@ function ContactCard(props) {
           <div>{email}</div>
         </Link>
       </div>
-      <i
-        className="trash alternate outline icon"
-        style={{ color: 'red', marginTop: '7px' }}
-        onClick={() => props.clickHandler(id)}
-      ></i>
+      <Link to="/delete">
+        <i
+          className="trash alternate outline icon"
+          style={{ color: 'red', marginTop: '7px' }}
+        ></i>
+      </Link>
     </div>
   );
 }
